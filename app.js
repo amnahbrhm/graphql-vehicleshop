27cc11e94bd53a9e4edcfb5c2a3d7b68de739256
AAlDughayshim <AAlDughayshim@worldofss.com>
2023-03-15 22:24:57 +0300

    inital

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const cors = require("cors");
 
 //connect to mongodb...
 mongoose.connect(`mongodb://127.0.0.1/vehicledb`);
@@ -11,14 +12,18 @@ const graphqlHTTP = require('express-graphql');
 const schema = require('./graphSchema/vehicleSchemaGQL');
 
 app.get('/', (req, res)=>{
-    res.end("graphql-vehicleshop is running on 3000 port...");
+    res.end("graphql-vehicleshop is running on 4000 port...");
 })
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
 
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true
 }))
 
-app.listen('3000', ()=>{
+
+app.listen('4000', ()=>{
     console.log("Server is starting on 3000 port...")
 })
